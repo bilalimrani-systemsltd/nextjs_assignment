11ceaf0de2a5385ce39737157580cd5af198ed23
bilalimrani <bilal.imrani@systemsltd.com>
2023-09-22 14:57:05 +0500

    assignment two

--- app/products/[slug]/not-found.tsx
+++ app/products/[slug]/not-found.tsx
@@ -5,7 +5,7 @@ export default function Error({ error }: { error: Error }) {
     <div className="h-screen bg-gray-200 flex flex-col justify-center items-center">
       <div className="bg-white px-9 py-14 shadow rounded">
         <h3 className="text-3xl font-bold">Well, this is embarrassing</h3>
-        <p className="text-reg font-bold">We couldn't find that product</p>
+        <p className="text-reg font-bold">We could not find that product</p>
         <p className="mt-6 text-sm font-light">Error Code: 404</p>
       </div>
     </div>
--- pages/api/brand/getbrand.ts
+++ pages/api/brand/getbrand.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   const data = await prisma.brand.findMany();
 
   res.status(200).json(data);
